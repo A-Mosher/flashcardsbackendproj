57e515178840193439154889d89b5f4b3952b74c
A-Mosher <adam.l.mosher@gmail.com>
2021-05-03 19:17:08 -0400

    added get all, get by id, put, delete, and validation into router functionality. tested with postman and all requests are functioning properl

--- routes/flashCards.js
+++ routes/flashCards.js
@@ -1,10 +1,39 @@
-const {Card} = require('../models/card');
-const {Collection} = require('../models/collection')
+const {Card, validate} = require('../models/card');
+//npmconst {Collection, validate} = require('../models/collection')
 const express = require('express');
+const { response } = require('express');
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const flashcards = await Card.find();
+        return res.send(flashcards);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+
+        const card = await Card.findById(req.params.id);
+
+        if (!card)
+            return res.status(400).send(`The card with id "${req.params.id}" does not exist.`);
+
+        return res.send(card);
+
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
 
 router.post('/', async (req, res) => {
     try {
+        const {error} = validate(req.body);
+        if (error)
+            return res.status(400).send(error);
+
         const card = new Card({
             category: req.body.category,
             question: req.body.question,
@@ -16,6 +45,45 @@ router.post('/', async (req, res) => {
         return res.send(card);
 
     }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const {error} = validate(req.bod);
+        if (error) return res.status(400).send (error);
+
+        const card = await Card.findByIdAndUpdate(req.params.id, {
+            category: req.body.category,
+            question: req.body.question,
+            answer: req.body.answer,
+        },
+        { new: true }
+        );
+
+        if (!card)
+            return res.status(400).send(`The card with id "${req.params.id}" does not exist.`);
+
+        await card.save();
+
+        return res.send(card);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        
+        const card = await Card.findByIdAndRemove(req.params.id);
+
+        if (!card)
+            return res.status(400).send(`The card with id "${req.params.id}" does not exist.`);
+         
+        return res.send(card);
+
+    }   catch(ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
